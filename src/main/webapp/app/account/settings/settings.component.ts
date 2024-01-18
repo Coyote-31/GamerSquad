cfd036ca9809dc63c0a6a1c26a0b6706a6773d7d
Coyote-31 <coyotepouil@gmail.com>
2024-01-18 16:12:48 +0100

    🎨 Update account settings
    - Update img url label
    - force identity to refresh on save settings

--- src/main/webapp/app/account/settings/settings.component.ts
+++ src/main/webapp/app/account/settings/settings.component.ts
@@ -49,7 +49,7 @@ export class SettingsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.accountService.identity().subscribe(account => {
+    this.accountService.identity(true).subscribe(account => {
       if (account) {
         this.settingsForm.patchValue(account);
       }
